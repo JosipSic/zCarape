80d724499063782408903abc52f826308822926d
JosipSic <josip_sic@yahoo.com>
2021-06-19 00:57:11 +0200

    Zavrsavam pivot izvestaj sa grafikonom

--- Modules/Proizvodnja/ViewModels/PivotViewModel.cs
+++ Modules/Proizvodnja/ViewModels/PivotViewModel.cs
@@ -216,4 +216,21 @@ namespace Proizvodnja.ViewModels
         }
     }
 
+    public class ValueSelectorItem : DependencyObject
+    {
+        public static readonly DependencyProperty ContentProperty = DependencyProperty.Register("Content", typeof(object), typeof(ValueSelectorItem));
+        public static readonly DependencyProperty DisplayNameProperty = DependencyProperty.Register("DisplayName", typeof(string), typeof(ValueSelectorItem));
+
+        public object Content
+        {
+            get { return GetValue(ContentProperty); }
+            set { SetValue(ContentProperty, value); }
+        }
+        public string DisplayName
+        {
+            get { return (string)GetValue(DisplayNameProperty); }
+            set { SetValue(DisplayNameProperty, value); }
+        }
+    }
+
 }
